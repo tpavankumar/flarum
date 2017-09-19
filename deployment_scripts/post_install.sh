f4ed67b64dc9707bfa4c451cf07798390beabd96
niranjan <niranjan.b@shawacademy.com>
2017-09-19 19:06:16 +0530

    code deploy

--- deployment_scripts/post_install.sh
+++ deployment_scripts/post_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo cp /home/ubuntu/flarum/deployment_scripts/flarum.ini /etc/php5/fpm/conf.d/gravity.ini
 sudo cp /home/ubuntu/flarum/deployment_scripts/nginx.conf /etc/nginx/nginx.conf
+cd /home/ubuntu/flarum
+php composer.phar install
+php flarum cache:clear
